7af3b9e20fa70d41f9e887de988fe39921461354
nahyunuk <s24022@gsm.hs.kr>
2024-10-29 17:22:04 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_organize_widget/home_page.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+
+
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }

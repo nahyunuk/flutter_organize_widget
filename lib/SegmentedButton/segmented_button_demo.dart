3591bd0d5402199f0f25f1e9ad1dce2626a98cca
nahyunuk <s24022@gsm.hs.kr>
2024-10-30 20:35:08 +0900

    페이지 생성

---
+++ lib/SegmentedButton/segmented_button_demo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SegmentedButtonDemo extends StatefulWidget {
+  const SegmentedButtonDemo({super.key});
+
+  @override
+  State<SegmentedButtonDemo> createState() => _SegmentedButtonDemoState();
+}
+
+class _SegmentedButtonDemoState extends State<SegmentedButtonDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        title:
+            Text("SegmentedButtonDemo", style: TextStyle(color: Colors.white)),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
